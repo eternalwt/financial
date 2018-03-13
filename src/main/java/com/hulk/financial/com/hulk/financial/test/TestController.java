6be0a792c1fc26da7f14ce07348a044ee114d66f
HulkGao <HulkGao@viatech.com.cn>
2018-03-13 12:23:03 +0800

    1.add logback; 2.add springboot port config in application.properties

--- src/main/java/com/hulk/financial/com/hulk/financial/test/TestController.java
+++ src/main/java/com/hulk/financial/com/hulk/financial/test/TestController.java
@@ -1,6 +1,8 @@
 package com.hulk.financial.com.hulk.financial.test;
 
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +16,13 @@ import javax.xml.ws.RequestWrapper;
 @Controller
 @RequestMapping("/")
 public class TestController {
+    public static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @ApiOperation(value="测试swagger2", notes="swagger2的第一段代码")
-    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
     @ResponseBody
     public String sayHello() {
+        logger.error("log output");
         return "Hello, world!";
     }
 }
